79f411345a56c7b097977815f1fb4b5149472ebf
LOU Xun <aquarhead@ela.build>
2019-02-25 20:44:35 +0000

    Slow down the ball!
    I'm old man T_T

--- src/pong.rs
+++ src/pong.rs
@@ -136,8 +136,8 @@ fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
     )
 }
 
-pub const BALL_VELOCITY_X: f32 = 75.0;
-pub const BALL_VELOCITY_Y: f32 = 50.0;
+pub const BALL_VELOCITY_X: f32 = 25.0;
+pub const BALL_VELOCITY_Y: f32 = 25.0;
 pub const BALL_RADIUS: f32 = 2.0;
 
 pub struct Ball {
