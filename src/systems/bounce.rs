60e42c27e4d6b66b4c3e3b530226b3243332c52f
LOU Xun <aquarhead@ela.build>
2019-02-25 21:05:56 +0000

    Refactor for lint warnings

--- src/systems/bounce.rs
+++ src/systems/bounce.rs
@@ -24,9 +24,9 @@ impl<'s> System<'s> for BounceSystem {
             let ball_y = transform.translation().y;
 
             // Bounce at the top or the bottom of the arena.
-            if ball_y >= ARENA_HEIGHT - ball.radius && ball.velocity[1] > 0.0 {
-                ball.velocity[1] = -ball.velocity[1];
-            } else if ball_y <= ball.radius && ball.velocity[1] < 0.0 {
+            if (ball_y >= ARENA_HEIGHT - ball.radius && ball.velocity[1] > 0.0)
+                || (ball_y <= ball.radius && ball.velocity[1] < 0.0)
+            {
                 ball.velocity[1] = -ball.velocity[1];
             }
 
@@ -40,19 +40,21 @@ impl<'s> System<'s> for BounceSystem {
                 // lowest coordinates, and adding the ball radius to the highest ones. The ball
                 // is then within the paddle if its centre is within the larger wrapper
                 // rectangle.
-                if point_in_rect(
+
+                let ball_collided = point_in_rect(
                     ball_x,
                     ball_y,
                     paddle_x - ball.radius,
                     paddle_y - ball.radius,
                     paddle_x + paddle.width + ball.radius,
                     paddle_y + paddle.height + ball.radius,
-                ) {
-                    if paddle.side == Side::Left && ball.velocity[0] < 0.0 {
-                        ball.velocity[0] = -ball.velocity[0];
-                    } else if paddle.side == Side::Right && ball.velocity[0] > 0.0 {
-                        ball.velocity[0] = -ball.velocity[0];
-                    }
+                );
+
+                let ball_velocity = (paddle.side == Side::Left && ball.velocity[0] < 0.0)
+                    || (paddle.side == Side::Right && ball.velocity[0] > 0.0);
+
+                if ball_collided && ball_velocity {
+                    ball.velocity[0] = -ball.velocity[0];
                 }
             }
         }
