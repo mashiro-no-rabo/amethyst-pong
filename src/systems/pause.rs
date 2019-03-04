e5a78b1947c836ca52b36cf7e68141314e212874
LOU Xun <aquarhead@ela.build>
2019-03-04 23:33:06 +0000

    Split pause and unpause to different action binding

--- src/systems/pause.rs
+++ src/systems/pause.rs
@@ -11,8 +11,11 @@ impl<'a> System<'a> for PauseSystem {
 
     fn run(&mut self, (mut time, input): Self::SystemData) {
         if let Some(true) = input.action_is_down("pause") {
-            let old_ts = time.time_scale();
-            &time.set_time_scale(1.0 - old_ts);
+            time.set_time_scale(0.0);
+        }
+
+        if let Some(true) = input.action_is_down("unpause") {
+            time.set_time_scale(1.0);
         }
     }
 }
